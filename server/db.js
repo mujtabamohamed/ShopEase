7b616eefa4a7b2545afb97c19ecb3bc888cf98b2
Mujtaba Mohamed <mujtabamohamed@gmail.com>
2024-09-16 22:30:39 +0530

    vercel

--- server/db.js
+++ server/db.js
@@ -4,17 +4,17 @@ import { config } from "dotenv";
 config();
 const { Pool } = pg;
 
-const pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PROCESS,
-});
-
 // const pool = new Pool({
-//     connectionString: process.env.POSTGRES_URL,
-//   })
+//     user: process.env.PG_USER,
+//     host: process.env.PG_HOST,
+//     database: process.env.PG_DATABASE,
+//     password: process.env.PG_PASSWORD,
+//     port: process.env.PG_PROCESS,
+// });
+
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+  })
 
 pool.connect((err) => {
     if(err) throw err
